4436bd4f503732cc825ccfd424127d9933a40b3d
unknown <hishammnkl@protonmail.com>
2020-11-26 18:29:50 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,91 @@
-import 'package:CovidVigilante/counter_bloc.dart';
-import 'package:CovidVigilante/counter_event.dart';
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyApp(
+    model: CounterModel(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
+  final CounterModel model;
+
+  const MyApp({Key key, @required this.model}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    // At the top level of our app, we'll, create a ScopedModel Widget. This
+    // will provide the CounterModel to all children in the app that request it
+    // using a ScopedModelDescendant.
+    return ScopedModel<CounterModel>(
+      model: model,
+      child: MaterialApp(
+        title: 'Scoped Model Demo',
+        home: CounterHome('Scoped Model Demo'),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+// Start by creating a class that has a counter and a method to increment it.
+//
+// Note: It must extend from Model.
+class CounterModel extends Model {
+  int _counter = 0;
 
-  final String title;
+  int get counter => _counter;
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+  void increment() {
+    // First, increment the counter
+    _counter++;
+
+    // Then notify all the listeners.
+    notifyListeners();
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final _bloc = CounterBloc();
+class CounterHome extends StatelessWidget {
+  final String title;
+
+  CounterHome(this.title);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
-        child: StreamBuilder<int>(
-            stream: _bloc.counter,
-            initialData: 0,
-            builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
-                    '${snapshot.data}',
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                ],
-              );
-            }),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            // Create a ScopedModelDescendant. This widget will get the
+            // CounterModel from the nearest parent ScopedModel<CounterModel>.
+            // It will hand that CounterModel to our builder method, and
+            // rebuild any time the CounterModel changes (i.e. after we
+            // `notifyListeners` in the Model).
+            ScopedModelDescendant<CounterModel>(
+              builder: (context, child, model) {
+                return Text(
+                  model.counter.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+          ],
+        ),
       ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () => _bloc.counterEventSink.add(DecrementEvent()),
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
-          ),
-          SizedBox(width: 10),
-          FloatingActionButton(
-            onPressed: () => _bloc.counterEventSink.add(IncrementEvent()),
+      // Use the ScopedModelDescendant again in order to use the increment
+      // method from the CounterModel
+      floatingActionButton: ScopedModelDescendant<CounterModel>(
+        builder: (context, child, model) {
+          return FloatingActionButton(
+            onPressed: model.increment,
             tooltip: 'Increment',
             child: Icon(Icons.add),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
